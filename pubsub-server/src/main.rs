78568fa7b007d0c57ea4f40c0a407dd13f6fbf50
Andriy Mykhaylyshyn <andriy.mykhaylyshyn@sigma.software>
2022-02-05 23:15:20 +0200

    Implementing ws server

--- pubsub-server/src/main.rs
+++ pubsub-server/src/main.rs
@@ -1,10 +1,15 @@
 mod pubsub;
-mod server;
 
 use clap::Parser;
 use dotenv::dotenv;
+use futures::StreamExt;
+use pubsub::PubSub;
 use std::net::{IpAddr, Ipv4Addr};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+use tokio_stream::wrappers::TcpListenerStream;
+use tokio_tungstenite::tungstenite::handshake::server::{
+    Request as HandshakeRequest, Response as HandshakeResponse,
+};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -13,17 +18,51 @@ struct Args {
     port: u16,
 }
 
+async fn handle_connection(
+    pubsub: PubSub<String, String>,
+    raw_stream: TcpStream,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let remote_addr = raw_stream.peer_addr()?;
+    let ws_stream = tokio_tungstenite::accept_hdr_async(
+        raw_stream,
+        |req: &HandshakeRequest, res: HandshakeResponse| {
+            log::info!("new connection from {} to {}", remote_addr, req.uri());
+            Ok(res)
+        },
+    )
+    .await?;
+    let (outgoing, incoming) = ws_stream.split();
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     let args: Args = Args::parse();
 
-    let listener = TcpListener::bind((IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), args.port)).await?;
+    let pubsub = PubSub::new(100);
+    let listener = TcpListenerStream::new(
+        TcpListener::bind((IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), args.port)).await?,
+    );
 
-    while let Ok((stream, addr)) = listener.accept().await {
-        log::info!("new connection from {:?}", addr);
-    }
+    listener
+        .for_each_concurrent(None, |stream_result| {
+            let pubsub = pubsub.clone();
+            async move {
+                match stream_result {
+                    Ok(stream) => {
+                        handle_connection(pubsub, stream)
+                            .await
+                            .map_err(|err| log::error!("handle connection error: {}", err))
+                            .ok();
+                    }
+                    Err(err) => log::error!("new connection error: {}", err),
+                }
+            }
+        })
+        .await;
 
     Ok(())
 }
