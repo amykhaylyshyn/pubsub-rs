bc01a0ebb357159f01682befc3d342d49391cf15
Andriy Mykhaylyshyn <andriy.mykhaylyshyn@sigma.software>
2022-02-05 23:26:19 +0200

    Implementing handling of WS upgrade request

--- pubsub-server/src/main.rs
+++ pubsub-server/src/main.rs
@@ -6,10 +6,12 @@ use futures::StreamExt;
 use pubsub::PubSub;
 use std::net::{IpAddr, Ipv4Addr};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::oneshot;
 use tokio_stream::wrappers::TcpListenerStream;
 use tokio_tungstenite::tungstenite::handshake::server::{
     Request as HandshakeRequest, Response as HandshakeResponse,
 };
+use tokio_tungstenite::tungstenite::http::uri::Uri;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -23,25 +25,28 @@ async fn handle_connection(
     raw_stream: TcpStream,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let remote_addr = raw_stream.peer_addr()?;
+    let (upgrade_tx, upgrade_rx) = oneshot::channel();
+    let (upgrade_result_tx, upgrade_result_rx) = oneshot::channel();
     let ws_stream = tokio_tungstenite::accept_hdr_async(
         raw_stream,
         |req: &HandshakeRequest, res: HandshakeResponse| {
             log::info!("new connection from {} to {}", remote_addr, req.uri());
+            upgrade_tx.send(req.uri().to_owned()).ok();
+            upgrade_result_rx.blocking_recv();
             Ok(res)
         },
     )
     .await?;
+
+    let upgrade_req_uri = upgrade_rx.await?;
+    upgrade_result_tx.send(());
+
     let (outgoing, incoming) = ws_stream.split();
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-
-    let args: Args = Args::parse();
-
+async fn run_server(args: Args) -> std::io::Result<()> {
     let pubsub = PubSub::new(100);
     let listener = TcpListenerStream::new(
         TcpListener::bind((IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), args.port)).await?,
@@ -65,4 +70,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await;
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let args: Args = Args::parse();
+    Ok(run_server(args).await?)
 }
