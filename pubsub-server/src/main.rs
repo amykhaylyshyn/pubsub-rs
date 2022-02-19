d0bdfcf3f62c1646a6bc9d30ba81a93fb8fc344e
Andriy Mykhaylyshyn <andriy.mykhaylyshyn@sigma.software>
2022-02-19 22:47:46 +0200

    Implemented Ctrl+C handling on windows

--- pubsub-server/src/main.rs
+++ pubsub-server/src/main.rs
@@ -4,8 +4,10 @@ mod server;
 use crate::server::Server;
 use clap::Parser;
 use dotenv::dotenv;
+use futures::{join, FutureExt};
 use pubsub::PubSub;
-use tokio::sync::watch;
+use std::io;
+use tokio::{signal, sync::watch};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -14,6 +16,16 @@ struct Args {
     port: u16,
     #[clap(long)]
     jwt_secret: String,
+}
+
+#[cfg(target_family = "windows")]
+async fn wait_exit_signal() -> io::Result<()> {
+    Ok(signal::ctrl_c().await?)
+}
+
+#[cfg(target_family = "unix")]
+async fn wait_exit_signal() -> io::Result<()> {
+    unimplemented!();
 }
 
 #[tokio::main]
@@ -26,5 +38,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let dispatch = PubSub::new(100);
     let server =
         Server::bind_to_port(args.port, args.jwt_secret.as_str(), dispatch, shutdown_rx).await?;
-    Ok(server.run().await?)
+    let wait_exit_fut = async move {
+        wait_exit_signal().await.expect("exit signal");
+        shutdown_tx.send(true).expect("shutdown signal sent");
+        log::info!("received shutdown signal");
+    }
+    .fuse();
+    let server_run_fut = async move {
+        server.run().await.expect("server running");
+    }
+    .fuse();
+
+    join!(wait_exit_fut, server_run_fut);
+
+    Ok(())
 }
